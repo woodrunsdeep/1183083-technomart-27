7ad9b3a0a129393058f562d830fd89598ee6a9a2
Gleb Dmitriev <woodrunsdeep@gmail.com>
2020-03-13 22:31:46 +0300

    запилил модалку добавления товара в корзину

--- js/script.js
+++ js/script.js
@@ -6,9 +6,13 @@ var popup = document.querySelector(".feedback-form");
 
 var modalMap = document.querySelector(".modal-map");
 
-var mapClose = modalMap.querySelector(".close-button");
+if (modalMap !== null) {
+  var mapClose = modalMap.querySelector(".close-button");
+} 
 
 var close = document.querySelector(".close-button");
+
+var proceedShopping = document.querySelector(".close-modal-button");
 
 var username = document.querySelector("[name=username]");
 
@@ -27,15 +31,20 @@ try {
   isStorageSupport = false;
 }
 
-mapLink.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modalMap.classList.add("modal-show");
-});
 
-mapClose.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modalMap.classList.remove("modal-show");
-});
+if (mapLink !== null) {
+  mapLink.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      modalMap.classList.add("modal-show");
+  });
+}
+
+if (mapLink !== null) {
+  mapClose.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      modalMap.classList.remove("modal-show");
+  });
+}
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
@@ -46,37 +55,44 @@ window.addEventListener("keydown", function (evt) {
     }
   });
 
-feedbackLink.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("modal-show");
+if (feedbackLink !== null) {
+  feedbackLink.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("modal-show");
 
-    if (storage) {
-        login.value = storage;
-    } else {
-        username.focus();
-    }
+      if (storage) {
+          login.value = storage;
+      } else {
+          username.focus();
+      }
 
-});
+  });
+}
 
-close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-    popup.classList.remove("modal-error");
-});
+if (popup !== null) {
+  close.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+  });
+}
 
-feedbackForm.addEventListener("submit", function(evt) {
-    if (!username.value || !email.value || !message.value) {
-        evt.preventDefault();
-        popup.classList.remove("modal-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("modal-error");
-    } else {
-        if (isStorageSupport) {
-            localStorage.setItem("username", username.value);
-        }
-    }
 
-});
+if (feedbackForm !== null) {
+  feedbackForm.addEventListener("submit", function(evt) {
+      if (!username.value || !email.value || !message.value) {
+          evt.preventDefault();
+          popup.classList.remove("modal-error");
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("modal-error");
+      } else {
+          if (isStorageSupport) {
+              localStorage.setItem("username", username.value);
+          }
+      }
+
+  });
+}
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
@@ -87,3 +103,32 @@ window.addEventListener("keydown", function (evt) {
       }
     }
   });
+
+var added = document.querySelector(".added-to-cart");
+
+var buttons = document.querySelectorAll(".button--to-cart")
+for (var button of buttons) {
+  button.addEventListener('click', function(evt) {
+    evt.preventDefault();
+		added.classList.add("added-to-cart--show");
+  })
+}
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  added.classList.remove("added-to-cart--show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (added.classList.contains("added-to-cart--show")) {
+      added.classList.remove("added-to-cart--show");
+    }
+  }
+});
+
+proceedShopping.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  added.classList.remove("added-to-cart--show");
+})
