a1271ccbbdbeadc0c1df626e306aa7efd15946b2
Gleb Dmitriev <woodrunsdeep@gmail.com>
2020-03-22 16:30:34 +0300

    полифил для closest()

--- js/script.js
+++ js/script.js
@@ -169,6 +169,18 @@ function findInd(array, target){
 return index;
 }
 
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
 slides.forEach(setSlidePosition);
 
 var moveToSlide = function(track, currentSlide, targetSlide) {
