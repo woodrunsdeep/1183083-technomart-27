02a3f5bceddd203cefbeb48f15fbb7e160ceacb1
Gleb Dmitriev <woodrunsdeep@gmail.com>
2020-03-22 15:44:27 +0300

    стрелочные функции тоже не работают в ie :)

--- js/script.js
+++ js/script.js
@@ -154,24 +154,24 @@ var dotsNav = document.querySelector(".carousel__nav");
 var dots = Array.prototype.slice.call(document.querySelectorAll(".carousel__indicator"));
 
 var slideWidth = slides[0].getBoundingClientRect().width;
-var setSlidePosition = (slide, index) => {
+var setSlidePosition = function(slide, index) {
     slide.style.left = slideWidth * index + "px";
 };
 
 slides.forEach(setSlidePosition);
 
-var moveToSlide = (track, currentSlide, targetSlide) => {
+var moveToSlide = function(track, currentSlide, targetSlide) {
   track.style.transform = "translateX(-" + targetSlide.style.left + ")";
   currentSlide.classList.remove("current-slide");
   targetSlide.classList.add("current-slide");
 };
 
-var updateDots = (currentDot, targetDot) => {
+var updateDots = function(currentDot, targetDot) {
   currentDot.classList.remove("current-slide");
   targetDot.classList.add("current-slide");
 };
 
-var hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+var hideShowArrows = function(slides, prevButton, nextButton, targetIndex) {
   if (targetIndex === 0) {
     prevButton.setAttribute("disabled", "");
     nextButton.removeAttribute("disabled", "");
@@ -184,7 +184,7 @@ var hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
   }
 };
 
-prevButton.addEventListener("click", e => {
+prevButton.addEventListener("click", function(e) {
   var currentSlide = track.querySelector(".current-slide");
   var prevSlide = currentSlide.previousElementSibling;
   var currentDot = dotsNav.querySelector(".current-slide");
@@ -196,7 +196,7 @@ prevButton.addEventListener("click", e => {
   hideShowArrows(slides, prevButton, nextButton, prevIndex);
 });
 
-nextButton.addEventListener("click", e => {
+nextButton.addEventListener("click", function(e) {
   var currentSlide = track.querySelector(".current-slide");
   var nextSlide = currentSlide.nextElementSibling;
   var currentDot = dotsNav.querySelector(".current-slide");
@@ -207,7 +207,7 @@ nextButton.addEventListener("click", e => {
   hideShowArrows(slides, prevButton, nextButton, nextIndex);
 });
 
-dotsNav.addEventListener("click", e => {
+dotsNav.addEventListener("click", function(e) {
   var targetDot = e.target.closest("button");
 
   if (!targetDot) return;
